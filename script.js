103fba605ea214cd286a5111be010978d8ef12df
josebergara <101436289+josebergara@users.noreply.github.com>
2022-03-19 10:38:37 -0300

    master

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 function compute() {
-    //Obtener los valores y calcular
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = (principal * years * rate) / 100;
-    var year = new Date().getFullYear() + parseInt(years);
-  
-    //Crear texto final
-    if (principal <= 0) {
-      alert("Please, enter a positive Number.");
-    } else {
-      document.getElementById("result").innerHTML =
-        "If you deposit " +
-        principal +
-        ",<br>at an interest rate of " +
-        rate +
-        "%<br>You will receive an amount of " +
-        interest +
-        ",<br>in the year " +
-        year +
-        "<br>";
-    }
+  //Obtener los valores y calcular
+  var principal = document.getElementById("principal").value;
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  var interest = (principal * years * rate) / 100;
+  var year = new Date().getFullYear() + parseInt(years);
+
+  //Crear texto final
+  if (principal <= 0) {
+    alert("Please, enter a positive Number.");
+  } else {
+    document.getElementById("result").innerHTML =
+      "If you deposit <mark>" +
+      principal +
+      "</mark>,<br>at an interest rate of <mark>" +
+      rate +
+      "%</mark><br>You will receive an amount of <mark>" +
+      interest +
+      "</mark>,<br>in the year " +
+      year +
+      "<br>";
   }
-  
-  //Función Tasa de Interes
-  
-  function getSliderValue() {
-    document.getElementById("rateSpan").innerHTML = document.getElementById(
-      "rate"
-    ).value;
-  }
-        
+}
+
+//Función Tasa de Interes
+
+function getSliderValue() {
+  document.getElementById("rateSpan").innerHTML = document.getElementById(
+    "rate"
+  ).value;
+}
